566fef05f57dbe7a4cf37fbb50bd82a816d5d697
Dan <daweidern@gmail.com>
2022-08-16 13:35:29 -0500

    commit tree ee4988b8e66682a270a7758a5d9a1d0c51b6fea4

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length, i !== j; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+return false;
 }
 
 /* 
